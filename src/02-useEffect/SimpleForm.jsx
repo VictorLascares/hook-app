33e29e6c3eafc19d6188ff0d49bc88b5b87d6a32
Victor Lascares <vmlascares@gmail.com>
2023-03-05 12:48:28 -0600

    useEffect

---
+++ src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+
+export const SimpleForm = () => {
+    const [formState, setFormState] = useState({
+        username: 'MaquinaAsesina1',
+        email: 'maquinaAsesina@correo.com'
+    });
+
+    const { username, email } = formState;
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormState({
+            ...formState,
+            [name]: value
+        })
+    }
+
+    useEffect(() => {
+        console.log("useEffect called!!");
+    }, [])
+
+    return (
+        <>
+            <h1>Simple Form</h1>
+            <hr />
+
+            <input
+                type="text"
+                className="form-control"
+                placeholder="Username"
+                name="username"
+                value={ username }
+                onChange={ handleChange }
+            />
+
+            <input
+                type="email"
+                className="form-control mt-2"
+                placeholder="email@example.com"
+                name="email"
+                value={ email }
+                onChange={ handleChange }
+            />
+        </>
+    );
+};
