6faed18be00d2aa8892faf564be54e696519bfbb
Victor Lascares <vmlascares@gmail.com>
2023-03-06 14:58:35 -0600

    Custom hook useFetch

---
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -0,0 +1,13 @@
+import { useFetch } from "../hooks/useFetch";
+
+export const MultipleCustomHooks = () => { 
+    const { data, isLoading, hasError } = useFetch('https://rickandmortyapi.com/api/character/1');
+
+    console.log({ data, isLoading, hasError });
+
+    return (
+        <>
+            <h1>Rick and Morty Characters</h1> 
+        </>
+    );
+}
