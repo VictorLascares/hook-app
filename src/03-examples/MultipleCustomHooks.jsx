67f21d49fb54cf135a13098d9b529b85a9b54e0d
Victor Lascares <vmlascares@gmail.com>
2023-03-06 17:53:13 -0600

    Custom hook useFetch con api de breakingbad

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,13 +1,32 @@
 import { useFetch } from "../hooks/useFetch";
 
-export const MultipleCustomHooks = () => { 
-    const { data, isLoading, hasError } = useFetch('https://rickandmortyapi.com/api/character/1');
+export const MultipleCustomHooks = () => {
+    const { data, isLoading, hasError } = useFetch('https://api.breakingbadquotes.xyz/v1/quotes/1');
 
-    console.log({ data, isLoading, hasError });
+    const { author, quote } = !!data && data[0];
 
     return (
         <>
-            <h1>Rick and Morty Characters</h1> 
+            <h1>BreakingBad Quotes</h1>
+            <hr />
+            {
+                isLoading
+                    ? (
+                        <div className="alert alert-info text-center">
+                            Loading...
+                        </div>
+                    )
+                    : (
+                        <blockquote className="blockquote text-end">
+                            <p className="mb-1">{ quote }</p>
+                            <footer className="blockquote-footer">{ author }</footer>
+                        </blockquote>
+                    )
+            }
+
+            <button className="btn btn-primary">
+                Next Quote
+            </button>
         </>
     );
 }
