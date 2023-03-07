7ab31e303d6271d70cada4670680fa8927b7b6f1
Victor Lascares <vmlascares@gmail.com>
2023-03-06 20:13:00 -0600

    useRef

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,11 @@ import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 import { SimpleForm } from './02-useEffect/SimpleForm'
 import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
+import { FocusScreen } from './04-useRef/FocusScreen'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <MultipleCustomHooks />
+    <FocusScreen />
   // </React.StrictMode>,
 )
