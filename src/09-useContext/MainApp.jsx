5894fa6844bdd41dad236fda50413487e2813e46
Victor Lascares <vmlascares@gmail.com>
2023-03-15 20:02:28 -0600

    Implementacion del componente Link de react-router-dom

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -2,12 +2,16 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import { HomePage } from "./HomePage";
 import { AboutPage } from "./AboutPage";
 import { LoginPage } from "./LoginPage";
+import { NavBar } from "./NavBar";
 
 export const MainApp = () => {
     return (
         <>
             <h1>MainApp</h1> 
+            <NavBar />
             <hr />
+
+            
 
             <Routes>
                 <Route path="/" element={ <HomePage /> } />
